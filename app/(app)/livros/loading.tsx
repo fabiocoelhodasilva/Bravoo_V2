b9b14e2bc85fa9d4eb6ba3568ce5f5c44cf83b43
Fabio Coelho da Silva <fabiocoelhodasilvaengenheiro@gmail.com>
2026-09-18 17:16:57 -0300

    dia

--- app/(app)/livros/loading.tsx
+++
@@ -1,5 +0,0 @@
-import JornadaLoading from "@/components/ui/JornadaLoading";
-
-export default function Loading() {
-  return <JornadaLoading active="livros" titulo="Minhas Leituras" />;
-}
--- app/(app)/meu-dia/loading.tsx
+++
@@ -1,5 +0,0 @@
-import JornadaLoading from "@/components/ui/JornadaLoading";
-
-export default function Loading() {
-  return <JornadaLoading active="meu-dia" titulo="Meu dia" />;
-}
